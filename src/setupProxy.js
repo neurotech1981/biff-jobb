ac8ec8959bfa0981f400863c08e539caa59be3fd
Bjørn-are Jakobsen <26271316+neurotech1981@users.noreply.github.com>
2019-01-06 00:54:59 +0100

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,5 @@
-const proxy = require('http-proxy-middleware')
+const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api', { target: 'https://127.0.0.1:5000/' }))
-  app.use(proxy('/api/getData', { target: 'http://127.0.0.1:5000/' }))
-  app.use(proxy('/api/putData', { target: 'http://127.0.0.1:5000/' }))
-  app.use(proxy('/api/deleteData', { target: 'http://127.0.0.1:5000/' }))
-  app.use(proxy('/api/updateData', { target: 'http://127.0.0.1:5000/' }))
-}
+  app.use(proxy("/api", { target: "http://localhost:5000/" }));
+};
